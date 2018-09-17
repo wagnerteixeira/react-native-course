20de1eef5ad4bd30b1a32df33b795cf5a5bc68f9
Wagner Bernardes Teixeira <wagnerbernardesteixeira@gmail.com>
2018-09-17 00:45:59 -0300

    Section 6, lesson 28.

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 /** @format */
-
-import {AppRegistry} from 'react-native';
+/*
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+*/
+import React from 'react';
+import { AppRegistry } from 'react-native';
+
+import Header from './src/components/header';
+
+const App = () => (
+    <Header headerText='Albums!' />
+);
+
+AppRegistry.registerComponent('albums', () => App);
+
