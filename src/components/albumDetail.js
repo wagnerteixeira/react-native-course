9f5d2cd3309e172f371cc0bd0b8cd0e1b2b01eb4
Wagner Bernardes Teixeira <wagnerbernardesteixeira@gmail.com>
2018-09-26 00:28:57 -0300

    section 8, lesson 54

--- src/components/albumDetail.js
+++ src/components/albumDetail.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 
 import Card from './card';
 import CardSection from './cardSection';
 import Button from './button';
 
 const AlbumDetail = ({ album }) => {
-    const { title, artist, thumbnail_image, image } = album;
+    const { title, artist, thumbnail_image, image, url } = album;
     const { 
         thumbnailStyle, 
         headerContentStyle, 
@@ -33,7 +33,9 @@ const AlbumDetail = ({ album }) => {
                 <Image style={imageStyle} source={{ uri: image }} />                
             </CardSection>
             <CardSection>
-                <Button onPress={() => console.log(title)} />
+                <Button onPress={() => Linking.openURL(url)}>
+                    Buy Now
+                </Button>
             </CardSection>
         </Card>
     );
